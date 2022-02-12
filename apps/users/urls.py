d4b9807f955089ca9095548cda341ca1c92a881a
vanessavps <vanessa.vps29@gmail.com>
2022-02-12 22:28:16 +1300

    Adding validation

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from apps.users import views
 
 urlpatterns = [
-    path('settings/user/<int:pk>', views.UserDetail.as_view()),
+    path('settings/user/<int:id>', views.UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,41 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import User
 from .serializers import UserSerializer
 
-class UserDetail(generics.RetrieveUpdateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+
+class UserDetail(APIView):
+    # GET method
+    # Returns user details given an id
+    # If id not found an error message with 404 status will be returned
+    def get(self, request, id):
+        try:
+            item = User.objects.get(id=id)
+        except User.DoesNotExist:
+            return Response({"error: User not found"}, status.HTTP_404_NOT_FOUND)
+
+        serializer = UserSerializer(item)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    # PUT method
+    # Updates user details given an id. It also updates details on external system
+    # Returns updated object
+    # If id not found an error message with 404 status will be returned
+    def put(self, request, id):
+        try:
+            item = User.objects.get(id=id)
+        except User.DoesNotExist:
+            return Response({"error: User not found"}, status.HTTP_404_NOT_FOUND)
+
+        # Partial is not accepted. The request data needs to have name and email
+        # If name or email is missing an error message with 400 status will be returned
+        serializer = UserSerializer(item, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            updateUserExternalSystem(serializer)
+            return Response(serializer.data, status.HTTP_200_OK)
+        else:
+            return Response({"error": serializer.errors}, status.HTTP_400_BAD_REQUEST)
