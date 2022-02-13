30dbaab01c325db5e9b650f9a5232d2395036ebe
vanessavps <vanessa.vps29@gmail.com>
2022-02-13 23:40:30 +1300

    Allow heroku host

--- vanessaProject/settings.py
+++ vanessaProject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-&bns&5j9r1-$6-$#oen(qy0cq-pm55d0f1y*8#4^#aal@b6-%l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vanessa-project.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
